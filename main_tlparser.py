853b84a742e92d0449da84f499f68df7e3ad7094
98031 <103958276+98031@users.noreply.github.com>
2022-04-19 23:03:45 +0530

    Update main_tlparser.py

--- main_tlparser.py
+++ main_tlparser.py
@@ -49,6 +49,18 @@ def main():
 
 if __name__ == '__main__':
     st.title("Streamlit web app")
+    main_bg = "tl_parser.jpg"
+    main_bg_ext = "jpg"
+    st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
     html_temp = """
     <div style="background-color:blue;padding:10px">
     <h2 style="color:white;text-align:center;">Time Log Parser App </h2>
